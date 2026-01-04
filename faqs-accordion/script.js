7474b9afa7f8cf126016cd88648bb93dcd2b5095
Isaac Dream <isaackprogramming44524d@gmail.com>
2026-01-04 17:22:17 +0200

    finished adding logic to the faq container

--- faqs-accordion/script.js
+++ faqs-accordion/script.js
@@ -2,4 +2,8 @@ const infoBox = document.querySelectorAll('.faq-container');
 //console.log(infoBox)
 const answer = document.querySelectorAll('.answer');
 //console.log(answer)
-
+for (let i = 0; i < infoBox.length; i++) {
+    infoBox[i].addEventListener('click', function() {
+        this.classList.toggle('active')
+    })
+}
