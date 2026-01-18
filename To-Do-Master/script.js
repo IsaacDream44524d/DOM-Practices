86ea9e70e1d309440100ea93d1387a02c405ecaa
Isaac Dream <isaackprogramming44524d@gmail.com>
2026-01-18 17:26:50 +0200

    finished the task rendering function and fixed duplication in task outputing

--- To-Do-Master/script.js
+++ To-Do-Master/script.js
@@ -1,21 +1,32 @@
-tasks = [
-    {
-        id: 1,
-        title: 'Finish my 5 projects by 25th of Jan'
-    },
 
-    {
-        id: 2,
-        title: 'Check Up on close friends'
+let tasks = [];
+
+const taskContainer = document.querySelector('.task-container');
+const addBtn = document.getElementById('add-btn');
+let inputBox = document.querySelector('#input');
+inputBox.innerHTML = '';
+
+
+addBtn.addEventListener('click', () => addTask());
+
+function addTask() {
+    inputValue = inputBox.value;
+    if (inputValue === '') return;
+    newTask = {
+        id: Date.now(),
+        title: inputValue
     }
-]
+    tasks.push(newTask);
+    viewAll()
+    inputBox.value = '';
+    console.log(tasks)
+}
 
-const taskContainer = document.querySelector('.task-container')
-
-function viewAll() {
+function viewAll(task) {
     // loop through the array to get the task append to a p in a div
     // add classlist to div to style
-    for (let i =0; i<tasks.length; i++) {
+    taskContainer.innerHTML ='';
+    for (let i = 0; i < tasks.length; i++) {
         let para = document.createElement('p');
         para.textContent = tasks[i].title;
         let taskList = document.createElement('div');
@@ -25,33 +36,30 @@ function viewAll() {
         buttons(taskList)
         //console.log(tasks[i].title)
     }
-    
-    // add buttons for task functionality
-    function buttons(taskList) {
-        // create butttons
-        const deleteBtn = document.createElement('button');
-        deleteBtn.classList.add('delete-btn')
-        const checkBtn = document.createElement('button');
-        checkBtn.classList.add('check-btn')
-        // append the buttons to the same div
-        const taskBtns = document.createElement('div')
-        taskBtns.classList.add('task-btns')
-        taskBtns.appendChild(deleteBtn)
-        taskBtns.appendChild(checkBtn)
-        // append the buttons div to the main div 
-        taskList.appendChild(taskBtns)
-
-        // append img to each button
-        const deleteImg = document.createElement('img');
-        deleteImg.src = 'assets/icons/recycle-bin.png';
-        deleteBtn.appendChild(deleteImg);
-
-        const checkImg = document.createElement('img');
-        checkImg.src = 'assets/icons/checked.png'
-        checkBtn.appendChild(checkImg);
-      
-
-    }
 }
 
-viewAll()
+// add buttons for task functionality
+function buttons(taskList) {
+    // create butttons
+    const deleteBtn = document.createElement('button');
+    deleteBtn.classList.add('delete-btn')
+    const checkBtn = document.createElement('button');
+    checkBtn.classList.add('check-btn')
+    // append the buttons to the same div
+    const taskBtns = document.createElement('div')
+    taskBtns.classList.add('task-btns')
+    taskBtns.appendChild(deleteBtn)
+    taskBtns.appendChild(checkBtn)
+    // append the buttons div to the main div 
+    taskList.appendChild(taskBtns)
+
+    // append img to each button
+    const deleteImg = document.createElement('img');
+    deleteImg.src = 'assets/icons/recycle-bin.png';
+    deleteBtn.appendChild(deleteImg);
+
+    const checkImg = document.createElement('img');
+    checkImg.src = 'assets/icons/checked.png'
+    checkBtn.appendChild(checkImg);
+}
+
